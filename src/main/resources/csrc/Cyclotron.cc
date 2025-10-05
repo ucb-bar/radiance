be99da75fc26d554c2fff69391e06b9c6334b174
Hansung Kim <hansung_kim@berkeley.edu>
2025-10-05 14:50:55 -0700

    Bump cyclotron to new DPI interface

--- src/main/resources/csrc/Cyclotron.cc
+++ src/main/resources/csrc/Cyclotron.cc
@@ -7,13 +7,12 @@
 extern "C" void cyclotron_init_rs(int num_lanes);
 extern "C" void cyclotron_init(int num_lanes) { cyclotron_init_rs(num_lanes); }
 
-extern "C" void cyclotron_tick_rs(const uint8_t *ibuf_ready,
-                                  uint8_t *ibuf_valid, uint32_t *ibuf_pc,
-                                  uint32_t *ibuf_op, uint32_t *ibuf_rd,
-                                  uint8_t *finished);
-extern "C" void cyclotron_tick(const uint8_t *ibuf_ready, uint8_t *ibuf_valid,
-                               uint32_t *ibuf_pc, uint32_t *ibuf_op,
-                               uint32_t *ibuf_rd, uint8_t *finished) {
-  cyclotron_tick_rs(ibuf_ready, ibuf_valid, ibuf_pc, ibuf_op, ibuf_rd,
-                    finished);
+extern "C" void cyclotron_get_trace_rs(const uint8_t *ready,
+                                    uint8_t *valid, uint32_t *pc,
+                                    uint32_t *op, uint32_t *rd,
+                                    uint8_t *finished);
+extern "C" void cyclotron_get_trace(const uint8_t *ready, uint8_t *valid,
+                                 uint32_t *pc, uint32_t *op,
+                                 uint32_t *rd, uint8_t *finished) {
+  cyclotron_get_trace_rs(ready, valid, pc, op, rd, finished);
 }
