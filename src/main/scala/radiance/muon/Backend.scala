eaaa84afbe5916168a9953fabec30019abf97dab
Hansung Kim <hansung_kim@berkeley.edu>
2025-10-23 18:06:54 -0700

    scoreboard: Multi-port, reset, fix partial row writes

--- src/main/scala/radiance/muon/Backend.scala
+++ src/main/scala/radiance/muon/Backend.scala
@@ -44,16 +44,25 @@ class Backend(implicit p: Parameters) extends CoreModule()(p) with HasCoreBundle
   io.ibuf.foreach(_.ready := true.B)
 
   val scoreboard = Module(new Scoreboard)
-  // TODO bogus
+  // TODO only looking at warp 0 ibuf
   // TODO merge scoreboard updates from RS admit + writeback
-  scoreboard.io.read.enable := true.B
-  scoreboard.io.read.pReg := 1.U
+  scoreboard.io.readRs1.enable := io.ibuf(0).valid
+  scoreboard.io.readRs1.pReg := io.ibuf(0).bits.inst.rs1
+  scoreboard.io.readRs2.enable := false.B
+  scoreboard.io.readRs2.pReg := DontCare
+  scoreboard.io.readRs3.enable := false.B
+  scoreboard.io.readRs3.pReg := DontCare
+  scoreboard.io.readRd.enable := false.B
+  scoreboard.io.readRd.pReg := DontCare
+  // TODO rs2-rd
+
   scoreboard.io.update.enable := true.B
   scoreboard.io.update.pReg := 1.U
   scoreboard.io.update.readInc := true.B
   scoreboard.io.update.readDec := false.B
   scoreboard.io.update.writeInc := false.B
   scoreboard.io.update.writeDec := true.B
+  dontTouch(scoreboard.io)
 
   // TODO: Issue queue
   // TODO: Collector
--- src/main/scala/radiance/muon/Scoreboard.scala
+++ src/main/scala/radiance/muon/Scoreboard.scala
@@ -29,15 +29,18 @@ class Scoreboard(implicit p: Parameters) extends CoreModule()(p) {
   val writeCountBits = 1 // 0 or 1
   val io = IO(new Bundle {
     // asynchronous-read, synchronous-write
-    val update = new ScoreboardWrite
-    val read = new ScoreboardRead(readCountBits, writeCountBits)
+    val update  = new ScoreboardWrite
+    val readRs1 = new ScoreboardRead(readCountBits, writeCountBits)
+    val readRs2 = new ScoreboardRead(readCountBits, writeCountBits)
+    val readRs3 = new ScoreboardRead(readCountBits, writeCountBits)
+    val readRd  = new ScoreboardRead(readCountBits, writeCountBits)
     // TODO: per-warp ports
   })
 
   def entryT = new Bundle {
     val pendingReads = UInt(readCountBits.W)
-    // TODO: epoch
     val pendingWrites = UInt(writeCountBits.W)
+    // TODO: reads epoch
   }
 
   // flip-flops
@@ -47,14 +50,29 @@ class Scoreboard(implicit p: Parameters) extends CoreModule()(p) {
   val maxPendingReadsU = muonParams.maxPendingReads.U
   val maxPendingWritesU = 1.U
 
-  // read
-  io.read.pendingReads := 0.U
-  io.read.pendingWrites := 0.U
-  when (io.read.enable) {
-    val row = table(io.read.pReg)
-    io.read.pendingReads := row.pendingReads
-    io.read.pendingWrites := row.pendingWrites
+  // reset
+  // @synthesis: unsure if this will generate expensive trees, revisit
+  when (reset.asBool) {
+    (0 until muonParams.numPhysRegs).foreach { pReg =>
+      table(pReg.U).pendingReads := 0.U
+      table(pReg.U).pendingWrites := 0.U
+    }
   }
+
+  // read
+  def read(port: ScoreboardRead) = {
+    port.pendingReads := 0.U
+    port.pendingWrites := 0.U
+    when (port.enable) {
+      val row = table(port.pReg)
+      port.pendingReads  := row.pendingReads
+      port.pendingWrites := row.pendingWrites
+    }
+  }
+  read(io.readRs1)
+  read(io.readRs2)
+  read(io.readRs3)
+  read(io.readRd)
 
   // update
   when (io.update.enable) {
@@ -63,24 +81,28 @@ class Scoreboard(implicit p: Parameters) extends CoreModule()(p) {
     assert(!(io.update.writeInc && io.update.writeDec),
            "scoreboard increment and decrement cannot be both asserted")
 
+    // partial writes to Mem rows seem to be flaky, construct a full row for
+    // writes instead
     val row = table(io.update.pReg)
+    val newRow = WireDefault(row)
     when (io.update.readInc === true.B) {
       when (row.pendingReads =/= maxPendingReadsU) {
-        row.pendingReads := row.pendingReads + 1.U
+        newRow.pendingReads := row.pendingReads + 1.U
       }
     }.elsewhen (io.update.readDec === true.B) {
       when (row.pendingReads =/= 0.U) {
-        row.pendingReads := row.pendingReads - 1.U
+        newRow.pendingReads := row.pendingReads - 1.U
       }
     }
     when (io.update.writeInc === true.B) {
       when (row.pendingWrites =/= maxPendingWritesU) {
-        row.pendingWrites := row.pendingWrites + 1.U
+        newRow.pendingWrites := row.pendingWrites + 1.U
       }
     }.elsewhen (io.update.writeDec === true.B) {
       when (row.pendingWrites =/= 0.U) {
-        row.pendingWrites := row.pendingWrites - 1.U
+        newRow.pendingWrites := row.pendingWrites - 1.U
       }
     }
+    table(io.update.pReg) := newRow
   }
 }
