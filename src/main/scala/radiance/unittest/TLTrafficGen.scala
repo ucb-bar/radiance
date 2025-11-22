5d33e40f9819be1e602abecb2ea64fbc5d3f6a11
Richard Yan <yrh@berkeley.edu>
2025-11-21 17:57:51 -0800

    add tilelink traffic generator

---
+++ src/main/scala/radiance/unittest/TLTrafficGen.scala
@@ -0,0 +1,156 @@
+package radiance.unittest
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+import freechips.rocketchip.diplomacy.IdRange
+import freechips.rocketchip.tilelink._
+import org.chipsalliance.cde.config.Parameters
+import org.chipsalliance.diplomacy.lazymodule._
+import radiance.memory.SourceGenerator
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+
+case class ScalaTLA(
+  address: BigInt,
+  lgSize: Int,
+  data: Option[BigInt], // none -> get, some -> put
+  mask: Option[Int]
+) {
+  def toChisel(params: TLBundleParameters): ChiselTLA = {
+    new ChiselTLA(params).Lit(
+      _.address -> address.U,
+      _.lgSize -> lgSize.U,
+      _.hasData -> data.isDefined.B,
+      _.hasMask -> mask.isDefined.B,
+      _.data -> data.fold(0.U)(_.U),
+      _.mask -> mask.fold(0.U)(_.U)
+    )
+  }
+}
+
+object ScalaTLA {
+  def apply(address: BigInt, lgSize: Int) =
+    new ScalaTLA(address, lgSize, None, None)
+
+  def apply(address: BigInt, lgSize: Int, data: BigInt) =
+    new ScalaTLA(address, lgSize, Some(data), None)
+
+  def apply(address: BigInt, lgSize: Int, data: BigInt, mask: Int) =
+    new ScalaTLA(address, lgSize, Some(data), Some(mask))
+}
+
+class ChiselTLA(params: TLBundleParameters) extends Bundle {
+  val address = UInt(params.addressBits.W)
+  val lgSize = UInt(params.sizeBits.W)
+  val data = UInt(params.dataBits.W)
+  val hasData = Bool()
+  val mask = UInt((params.dataBits / 8).W)
+  val hasMask = Bool()
+}
+
+class TLTrafficGen(val nodeName: String, val sourceBits: Int,
+                   val n: Int, val reqFn: Int => ScalaTLA)
+                  (implicit p: Parameters) extends LazyModule {
+
+  val node = TLClientNode(Seq(TLMasterPortParameters.v2(
+    masters = Seq(TLMasterParameters.v2(
+      name = nodeName,
+      sourceId = IdRange(0, 1 << sourceBits)
+    )),
+  )))
+
+  lazy val module = new LazyModuleImp(this) {
+    // elaboration time
+
+    val (tlNode, tlEdge) = node.out.head
+    val scalaReqs = Seq.tabulate(n)(reqFn)
+    val reqData = scalaReqs.map(_.toChisel(tlNode.params))
+    val storage = Module(new StoredBitVec(nodeName, reqData))
+
+    // run time
+
+    val sourceGen = Module(new SourceGenerator(sourceBits))
+    val (counter, ended) = Counter(tlNode.a.fire, n)
+
+    val finished = IO(Output(Bool()))
+    val finishedReg = RegEnable(true.B, false.B, ended)
+
+    storage.io.addr := counter
+    val storedReq = storage.io.data
+
+    tlNode.a.bits := Mux(
+      storedReq.hasData,
+      Mux(
+        storedReq.hasMask,
+        tlEdge.Put(sourceGen.io.id.bits, storedReq.address, storedReq.lgSize,
+          storedReq.data, storedReq.mask)._2,
+        tlEdge.Put(sourceGen.io.id.bits, storedReq.address, storedReq.lgSize,
+          storedReq.data)._2
+      ),
+      tlEdge.Get(sourceGen.io.id.bits, storedReq.address, storedReq.lgSize)._2,
+    )
+
+    sourceGen.io.gen := tlNode.a.ready && !finishedReg
+    tlNode.a.valid := sourceGen.io.id.valid && !finishedReg
+
+    tlNode.d.ready := true.B
+    sourceGen.io.reclaim.valid := tlNode.d.fire
+    sourceGen.io.reclaim.bits := tlNode.d.bits.source
+
+    finished := finishedReg && !sourceGen.io.inflight
+
+    dontTouch(tlNode.d)
+  }
+}
+
+class StoredBitVec[T <: Bundle](val filename: String, val sequence: Seq[T])
+                               (implicit p: Parameters) extends Module {
+  val addrWidth = log2Ceil(sequence.length)
+  val filePath = Paths.get(s"./generators/radiance/src/test/resources/traffic/${filename}.mem")
+  val fileContent = sequence.map { record =>
+    record.litOption.get.toString(16) // dontcares -> 0
+  }.mkString("\n")
+  Files.write(filePath, fileContent.getBytes(StandardCharsets.UTF_8))
+
+  val bb = Module(new StoredBitVecBlackBox(
+    filePath.toAbsolutePath.toString, addrWidth, sequence.length, sequence.head.cloneType.getWidth
+  ))
+  val io = IO(new Bundle {
+    val addr = Input(UInt(addrWidth.W))
+    val data = Output(sequence.head.cloneType)
+  })
+  bb.io.addr := io.addr
+  io.data := bb.io.data.asTypeOf(sequence.head.cloneType)
+}
+
+class StoredBitVecBlackBox(filePath: String, addrWidth: Int, seqLength: Int, dataWidth: Int)
+  extends BlackBox(Map(
+    "FILE_PATH" -> filePath,
+    "ADDR_WIDTH" -> addrWidth,
+    "DATA_WIDTH" -> dataWidth,
+    "SEQ_LENGTH" -> seqLength,
+  )) with HasBlackBoxInline {
+
+  val io = IO(new Bundle {
+    val addr = Input(UInt(addrWidth.W))
+    val data = Output(UInt(dataWidth.W))
+  })
+  val moduleName = this.getClass.getSimpleName
+
+  setInline(s"$moduleName.v",
+    s"""
+       |module $moduleName #(parameter FILE_PATH="", ADDR_WIDTH=0, DATA_WIDTH=0, SEQ_LENGTH=0) (
+       |    input  [ADDR_WIDTH-1:0] addr,
+       |    output [DATA_WIDTH-1:0] data);
+       |
+       |    logic [DATA_WIDTH-1:0] stimuli [0:SEQ_LENGTH-1];
+       |    initial begin
+       |        $$readmemh(FILE_PATH, stimuli);
+       |    end
+       |
+       |    assign data = stimuli[addr];
+       |endmodule
+       |""".stripMargin)
+}
