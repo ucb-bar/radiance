39c7bdb69ef280fb27f10d8f13d51f2e0d0af8bc
Hansung Kim <hansung_kim@berkeley.edu>
2026-01-06 00:27:22 -0800

    ci: Re-enable conda env caching with stable path

--- .github/scripts/setup-chipyard.sh
+++ .github/scripts/setup-chipyard.sh
@@ -11,11 +11,13 @@ cd "${CHIPYARD_DIR}"
 
 if [[ "${CACHE_HIT}" == "true" ]]; then
     echo "[CI] Cache hit! Skipping conda and toolchain steps"
-    export RISCV="${CHIPYARD_DIR}/.conda-env/riscv-tools"
+    export RISCV="${CHIPYARD_CONDA_ENV_PATH}/riscv-tools"
     [ -d "${RISCV}" ] || ( echo "error: ${RISCV} does not exist!" && exit 1 )
     source $(conda info --base)/etc/profile.d/conda.sh
-    conda activate "${CHIPYARD_DIR}/.conda-env"
-    ./build-setup.sh riscv-tools --skip-conda --skip-toolchain --skip-precompile --skip-firesim --skip-marshal
+    conda activate "${CHIPYARD_CONDA_ENV_PATH}"
+    ./build-setup.sh riscv-tools --conda-env-name "${CHIPYARD_CONDA_ENV_NAME}" \
+        --skip-conda --skip-toolchain --skip-precompile --skip-firesim --skip-marshal
 else
-    ./build-setup.sh riscv-tools --skip-precompile --skip-firesim --skip-marshal
+    ./build-setup.sh riscv-tools --conda-env-name "${CHIPYARD_CONDA_ENV_NAME}" \
+        --skip-precompile --skip-firesim --skip-marshal
 fi
