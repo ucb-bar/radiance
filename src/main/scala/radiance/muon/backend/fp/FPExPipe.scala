85890a3c909087245d7b14f198ab6a621feff272
Shashank Anand <shashank.anand@berkeley.edu>
2026-02-09 19:21:46 -0800

    backend: fpEX pipe

--- src/main/scala/radiance/muon/backend/fp/FPExPipe.scala
+++ src/main/scala/radiance/muon/backend/fp/FPExPipe.scala
@@ -6,6 +6,7 @@ import chisel3.util.experimental.decode._
 import org.chipsalliance.cde.config.Parameters
 import radiance.muon._
 import radiance.muon.backend._
+import fpex._
 
 class FpExOpBundle extends Bundle {
   val neg = Bool()
@@ -30,4 +31,49 @@ object FpExOpDecoder {
     result.roundingMode := FPRoundingMode.safe(f3)._1
     result
   }
+}
+
+class FPExPipe(fmt: FPFormat.Type)
+  (implicit p: Parameters)
+  extends ExPipe(
+    decomposerTypes = Some(Seq(UInt(p(MuonKey).archLen.W), Bool())),
+    recomposerTypes = Some(Seq(UInt(p(MuonKey).archLen.W), Bool())),
+    outLanes = Some(p(MuonKey).fpPipe.numFP16ExpLanes),
+    writebackSched = false, writebackReg = true, requiresRs3 = true)
+    with HasFPPipeParams {
+  val fpEX = Module(new FPEX(FPType.BF16T, numFP16ExpLanes, fpEXTagBits))
+
+  val ioFpExOp = FpExOpDecoder.decode(inst(Opcode), inst(F3), inst(F7), inst(Rs2))
+  val req = RegEnable(ioFpExOp, 0.U.asTypeOf(new FpExOpBundle), io.req.fire)
+  val fpExReq = Mux(io.req.fire, ioFpExOp, req)
+
+  val fpEXRespRd = RegEnable(fpEX.io.resp.bits.tag(Isa.regBits - 1, 0), 0.U(Isa.regBits.W),
+    fpEX.io.resp.valid)
+  val signExtFP16FpEXRes = signExtendFp16Lanes(numFP16ExpLanes, fpEX.io.resp.bits.result.asUInt)
+
+  io.req.ready := decomposer.get.io.in.ready
+  decomposer.get.io.in.valid := io.req.fire
+  decomposer.get.io.in.bits.data(0) := io.req.bits.rs1Data.get
+  decomposer.get.io.in.bits.data(1) := VecInit(io.req.bits.uop.tmask.asBools)
+  decomposer.get.io.out.ready := fpEX.io.req.ready
+
+  fpEX.io.req.valid := decomposer.get.io.out.valid
+  fpEX.io.req.bits.roundingMode := fpExReq.roundingMode.asUInt
+  fpEX.io.req.bits.tag := Mux(io.req.fire, inst(Rd), reqRd)
+  fpEX.io.req.bits.neg := fpExReq.neg
+  fpEX.io.req.bits.xVec := VecInit(decomposer.get.io.out.bits.data(0).map(reg =>
+      reg.asUInt(fpWordBits(fmt) - 1, 0)))
+  fpEX.io.req.bits.laneMask := decomposer.get.io.out.bits.data(1)
+  fpEX.io.resp.ready := recomposer.get.io.in.ready
+
+  recomposer.get.io.in.valid := fpEX.io.resp.valid
+  recomposer.get.io.in.bits.data(0) := signExtFP16FpEXRes
+  recomposer.get.io.in.bits.data(1) := fpEX.io.resp.bits.tag
+  recomposer.get.io.out.ready := io.resp.ready
+
+  io.resp.valid := recomposer.get.io.out.valid
+  io.resp.bits.reg.get.valid := recomposer.get.io.out.valid
+  io.resp.bits.reg.get.bits.rd := fpEXRespRd
+  io.resp.bits.reg.get.bits.data := recomposer.get.io.out.bits.data(0)
+  io.resp.bits.reg.get.bits.tmask := recomposer.get.io.out.bits.data(1).asUInt
 }
