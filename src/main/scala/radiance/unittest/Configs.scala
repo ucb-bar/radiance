ab8c00a707772f4283b35644d25d8e4826bf4220
joshua <jyou12@berkeley.edu>
2026-02-13 14:12:04 -0800

    add config for long tests

--- src/main/scala/radiance/unittest/Configs.scala
+++ src/main/scala/radiance/unittest/Configs.scala
@@ -33,6 +33,12 @@ class MuonCoreTestConfig extends Config(
   new WithSIMTConfig(numWarps = 8, numLanes = 16, numLsuLanes = 16, numSMEMInFlights = 4) ++
   new BaseSubsystemConfig)
 
+class MuonCoreLongTestConfig extends Config(
+  new WithMuonUnitTestHarness(new MuonCoreTest(timeout = 10000000)(_)) ++
+  new WithMuonCores(1, standalone = true, difftest = true) ++
+  new WithSIMTConfig(numWarps = 8, numLanes = 16, numLsuLanes = 16, numSMEMInFlights = 4) ++
+  new BaseSubsystemConfig)
+
 class MuonCoreNoILPTestConfig extends Config(
   new WithMuonUnitTestHarness(new MuonCoreTest()(_)) ++
   new WithMuonCores(1, standalone = true, noILP = true, difftest = true) ++
