51d1eed46bf7ca7351970d3ee3e9710461ca36df
Hansung Kim <hansung_kim@berkeley.edu>
2026-03-18 13:14:42 -0700

    lsu: load/store meta+data depth 16

--- src/main/scala/radiance/muon/LSU.scala
+++ src/main/scala/radiance/muon/LSU.scala
@@ -12,12 +12,12 @@ case class LoadStoreUnitParams(
     val numLsuLanes: Int = 16, // width of downstream memory interface and writeback; width of execute fixed to # of lanes
     
     val numGlobalLdqEntries: Int = 16, // limited to 8 decoded global load insts per warp
-    val numGlobalStqEntries: Int = 8, // limited to 4 decoded global store insts per warp
+    val numGlobalStqEntries: Int = 16, // limited to 4 decoded global store insts per warp
     val numSharedLdqEntries: Int = 16, // limited to 8 decoded shared load insts per warp
-    val numSharedStqEntries: Int = 8, // limited to 8 decoded shared store insts per warp
+    val numSharedStqEntries: Int = 16, // limited to 8 decoded shared store insts per warp
 
     val loadDataEntries: Int = 16, // limited to 16 in-flight / waiting to writeback load requests
-    val storeDataEntries: Int = 8, // limited to 8 unissued store requests
+    val storeDataEntries: Int = 16, // limited to 8 unissued store requests
     val addressEntries: Int = 16,  // limited to 16 unissued memory requests
 
     // purely performance optimizations; turn off if bad things are happening
